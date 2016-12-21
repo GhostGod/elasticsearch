b5419dfba1b9d22c0f22c63952b3dea422236e6f
liuyang <y_liu@hiersun.com>
2016-12-21 16:55:27 +0800

    添加控制器

--- src/main/java/com/liuyang/test/Test.java
+++ src/main/java/com/liuyang/test/Test.java
@@ -18,17 +18,27 @@ public class Test {
 
 	public static void main(String[] args) {
 		RestTemplate restTemplate = new RestTemplate();
+		boolean found = false;
 		//查询是否存在
+		logger.info("简单查询");
 		ResponseEntity<Body1> responseEntity = restTemplate.exchange("http://192.168.59.103:9200/megacorp/employee/1", HttpMethod.GET, null, Body1.class);
 		if(responseEntity.getStatusCodeValue() == HttpStatus.OK.value()){
 			Body1 body = responseEntity.getBody();
-			if(body.isFound()){
-				logger.debug("found /megacorp/employee/1.");
-				logger.debug("stop put");
-				return;
-			}
+			found = body.isFound();
 		}
 		
+		String body1 = "{\"query\" : {\"match\" : {\"last_name\" : \"Smith\"}}}";
+		HttpEntity<String> requestEntity1 = new HttpEntity<String>(body1);
+		ResponseEntity<String> responseEntity1 = restTemplate.exchange("http://192.168.59.103:9200/megacorp/employee/_search", HttpMethod.GET, requestEntity1, String.class);
+		if(responseEntity1.getStatusCodeValue() == HttpStatus.OK.value()){
+			logger.debug(responseEntity1.getBody());
+		}
+		
+		if(found){
+			logger.debug("found /megacorp/employee/1");
+			logger.debug("stop put");
+			return;
+		}
 		
 		JSONObject body = new JSONObject();
 		body.put("first_name", "John");
